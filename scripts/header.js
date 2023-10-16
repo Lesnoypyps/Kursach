99818e1e84da29b05726c9be8ad28a50e25ecdf3
Lesnoy_Pypsik <mishustin.y@mail.ru>
2023-10-16 10:39:23 +0300

    new Version

---
+++ scripts/header.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const header = document.querySelector('.header');
+
+const logo = document.createElement('div');
+const logoText = document.createElement('p');
+const logoIcon = document.createElement('img')
