12592801d54e20afde398229bc9276d1e600a5ce
Lesnoy_Pypsik <mishustin.y@mail.ru>
2023-10-25 23:22:39 +0300

    add script for capital and description

--- scripts/slides.js
+++ scripts/slides.js
@@ -12,10 +12,47 @@ let datasetSlides = [
     'image/fiji-island.jpg',
     'image/fiji-island.jpg',
 ];
+let dataSetSlidesText = [
+    {
+        city:'Stambol',
+        description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
+            'Dolore excepturi iusto nulla. ' +
+            'Distinctio doloremque exercitationem explicabo harum impedit in iste magnam, modi neque nesciunt nihil repudiandae sint sit unde ut?',
+    },
+    {
+        city:'Saint Petersburg',
+        description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
+            'Dolore excepturi iusto nulla. ' +
+            'Distinctio doloremque exercitationem explicabo harum impedit in iste magnam, modi neque nesciunt nihil repudiandae sint sit unde ut?',
+    },
+    {
+        city:'Moscow',
+        description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
+            'Dolore excepturi iusto nulla. ' +
+            'Distinctio doloremque exercitationem explicabo harum impedit in iste magnam, modi neque nesciunt nihil repudiandae sint sit unde ut?',
+    },
+    {
+        city:'New York',
+        description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
+            'Dolore excepturi iusto nulla. ' +
+            'Distinctio doloremque exercitationem explicabo harum impedit in iste magnam, modi neque nesciunt nihil repudiandae sint sit unde ut?',
+    },
+    {
+        city:'Paris',
+        description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. ' +
+            'Dolore excepturi iusto nulla. ' +
+            'Distinctio doloremque exercitationem explicabo harum impedit in iste magnam, modi neque nesciunt nihil repudiandae sint sit unde ut?',
+    }
+
+]
 
 for (let i = 0; i < slides.length; i++) {
     slides[i].dataset.id = i;
     slides[i].style.backgroundImage = `url(${datasetSlides[i]})`;
+    const capital = slides[i].querySelector('.capital-name');
+    const country = slides[i].querySelector('.country-name');
+    capital.textContent = dataSetSlidesText[i].city;
+    country.textContent = dataSetSlidesText[i].description;
 }
 
 function showPreviousSlide() {
